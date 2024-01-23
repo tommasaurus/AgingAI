595708e6d1c4de7ee0f1dfa92e90ddde634bca5a
Tommy Qu <rck9ng@virginia.edu>
2024-01-23 15:15:09 -0500

    created discriminator class

--- models.py
+++ models.py
@@ -63,3 +63,33 @@ class Generator(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+
+class Discriminator(nn.Module):
+    def __init__(self, ndf):
+        super(Discriminator, self).__init__()
+
+        # A bunch of convolutions one after another
+        model = [nn.Conv2d(3, ndf, 4, stride=2, padding=1),
+                 nn.LeakyReLU(0.2, inplace=True)]
+
+        model += [nn.Conv2d(ndf, ndf * 2, 4, stride=2, padding=1),
+                  nn.BatchNorm2d(ndf * 2),
+                  nn.LeakyReLU(0.2, inplace=True)]
+
+        model += [nn.Conv2d(ndf * 2, ndf * 4, 4, stride=2, padding=1),
+                  nn.InstanceNorm2d(ndf * 4),
+                  nn.LeakyReLU(0.2, inplace=True)]
+
+        model += [nn.Conv2d(ndf * 4, ndf * 8, 4, padding=1),
+                  nn.InstanceNorm2d(ndf * 8),
+                  nn.LeakyReLU(0.2, inplace=True)]
+
+        # FCN classification layer
+        model += [nn.Conv2d(ndf * 8, 1, 4, padding=1)]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        x = self.model(x)
+        # Average pooling and flatten
+        return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
