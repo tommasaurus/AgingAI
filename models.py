12226242ecaca03ec6358501a20c6f709ee31fe5
Tommy Qu <rck9ng@virginia.edu>
2024-01-23 15:11:35 -0500

    created a generator class

--- models.py
+++ models.py
@@ -1,22 +1,65 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class ResidualBlock(nn.Module):
     # To combat vanishing gradient
-    def __init__(self, in_features):
+    def __init__(self, num_features):
         super(ResidualBlock, self).__init__()
-
-        conv_block = [nn.ReflectionPad2d(1),
-                      nn.Conv2d(in_features, in_features, 3),
-                      nn.BatchNorm2d(in_features),
+        padding_size = 1
+        conv_block = [nn.ReflectionPad2d(padding_size),
+                      nn.Conv2d(num_features, num_features, 3),
+                      nn.BatchNorm2d(num_features),
                       nn.ReLU(),
-                      nn.ReflectionPad2d(1),
-                      nn.Conv2d(in_features, in_features, 3),
-                      nn.BatchNorm2d(in_features)]
+                      nn.ReflectionPad2d(padding_size),
+                      nn.Conv2d(num_features, num_features, 3),
+                      nn.BatchNorm2d(num_features)]
 
         self.conv_block = nn.Sequential(*conv_block)
 
     def forward(self, x):
         return x + self.conv_block(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, ngf, n_residual_blocks=9):
+        super(Generator, self).__init__()
+
+        # Initial convolution block
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(3, ngf, 7),
+                 nn.BatchNorm2d(ngf),
+                 nn.ReLU()]
+
+        # Downsampling
+        in_features = ngf
+        out_features = in_features * 2
+        for _ in range(2):
+            model += [nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+                      nn.BatchNorm2d(out_features),
+                      nn.ReLU()]
+            in_features = out_features
+            out_features = in_features * 2
+
+        # Residual blocks
+        for _ in range(n_residual_blocks):
+            model += [ResidualBlock(in_features)]
+
+        # Upsampling
+        out_features = in_features // 2
+        for _ in range(2):
+            model += [nn.ConvTranspose2d(in_features, out_features, 3, stride=2, padding=1, output_padding=1),
+                      nn.BatchNorm2d(out_features),
+                      nn.ReLU()]
+            in_features = out_features
+            out_features = in_features // 2
+
+        # Output layer
+        model += [nn.ReflectionPad2d(3),
+                  nn.Conv2d(ngf, 3, 7),
+                  nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
 
